0c572184d1b3d306713d9b290df02bcfd45b3cbe
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-08 17:57:00 +0700

    Create Product.cs

---
+++ api-project/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_project.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
