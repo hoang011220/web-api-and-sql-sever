7ff96d64eaca5fe8fd3e974dc75ac9fd7523e243
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-10 00:06:06 +0700

    Update Customer.cs

--- api-project/Models/Customer.cs
+++ api-project/Models/Customer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace api_project.Models
+﻿namespace api_project.Models
 {
     public class Customer
     {
