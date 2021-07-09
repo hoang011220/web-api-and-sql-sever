1a72dc9e0746508f2da02d642b2c054a9876e963
hoang011220 <71324252+hoang011220@users.noreply.github.com>
2021-07-10 00:06:01 +0700

    Create CustomerController.cs

---
+++ api-project/Controllers/CustomerController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using api_project.Models;
+namespace api_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select CustomerID, CustomerName from dbo.Customer";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ProjectApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+        }
+
+        [HttpPost]
+        public JsonResult Post(Customer cus)
+        {
+            string query = @"
+                insert into dbo.Customer
+                values (@CustomerName)
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ProjectApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@CustomerName", cus.CustomerName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully!!");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Customer cus)
+        {
+            string query = @"
+                update dbo.Customer
+                set CustomerName = @CustomerName
+                where CustomerId = @CustomerId
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ProjectApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@CustomerId", cus.CustomerId);
+                    myCommand.Parameters.AddWithValue("@CustomerName", cus.CustomerName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Update Successfully!!");
+
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                delete from dbo.Customer
+                where CustomerId = @CustomerId
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ProjectApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@CustomerId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Delete Successfully!!");
+
+        }
+    }
+}
